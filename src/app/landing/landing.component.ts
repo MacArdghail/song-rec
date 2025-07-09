04e69ac320fb2eec226dddc0bd16a0701d72cfdf
Fionn McArdle <f.mcardle1@universityofgalway.ie>
2025-07-09 19:22:43 +0100

    [Frontend]: Landing page template

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,6 +1,5 @@
-import { Component, inject } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { environment } from '../../environments/environment';
+import { Component } from '@angular/core';
+
 @Component({
   selector: 'app-landing',
   standalone: true,
@@ -8,24 +7,4 @@ import { environment } from '../../environments/environment';
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css',
 })
-export class LandingComponent {
-  apiUrl = environment.apiBaseUrl;
-
-  private http = inject(HttpClient);
-  message: string = '';
-  ngOnInit() {
-    this.retrieveMessage();
-  }
-
-  retrieveMessage() {
-    this.http.get<any>(`${this.apiUrl}/test-db`).subscribe({
-      next: (response) => {
-        this.message = response.testMessage;
-      },
-      error: (err) => {
-        console.error('Failed to fetch message', err);
-        this.message = 'Something went wrong. Please try again later';
-      },
-    });
-  }
-}
+export class LandingComponent {}
