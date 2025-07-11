f005c47721fd890420708e636d377d64cc421bec
Fionn McArdle <f.mcardle1@universityofgalway.ie>
2025-07-11 23:08:54 +0100

    [frontend]: testing with local backend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  apiBaseUrl: 'https://song-rec-a3e5d92b3f5c.herokuapp.com',
-  // apiBaseUrl: 'http://localhost:3000',
+  // apiBaseUrl: 'https://song-rec-a3e5d92b3f5c.herokuapp.com',
+  apiBaseUrl: 'http://localhost:3000',
 };
